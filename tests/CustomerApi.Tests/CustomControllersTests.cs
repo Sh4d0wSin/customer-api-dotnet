7a43311b09989290aa5d26541a6025df6e5577a8
Sh4d0wSin <s.wnarula@outlook.com>
2026-03-24 14:53:47 +0100

    Add integration tests for POST and GET-by-ID endpoints

--- tests/CustomerApi.Tests/CustomControllersTests.cs
+++ tests/CustomerApi.Tests/CustomControllersTests.cs
@@ -28,4 +28,46 @@ public class CustomersControllerTests : IClassFixture<CustomWebApplicationFactor
 
 
     }
+
+    [Fact]
+    public async Task Post_Customers_ReturnsCreated() {
+        Customer new_customer = new Customer {Name = "Michael", Email = "myers@testmail.de"};
+        
+        var response = await _client.PostAsJsonAsync("/api/customers", new_customer);
+
+        
+
+        var customer = await response.Content.ReadFromJsonAsync<Customer>();
+
+        Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        Assert.NotNull(customer);
+        Assert.Equal("Michael", customer.Name);
+        Assert.Equal("myers@testmail.de", customer.Email);
+        Assert.True(customer.Id > 0);
+
+
+    }
+
+    [Fact]
+    public async Task Get_HappyCustomers_ReturnsId() {
+        Customer new_customer = new Customer {Name = "Happy", Email = "happy@testmail.de"};
+        
+        var response = await _client.PostAsJsonAsync("/api/customers", new_customer);
+
+        var customer = await response.Content.ReadFromJsonAsync<Customer>();
+        Assert.NotNull(customer);
+
+
+
+        var createdCustomer = await _client.GetAsync($"/api/customers/{customer.Id}");
+
+        var fetchedCustomer = await createdCustomer.Content.ReadFromJsonAsync<Customer>();
+
+        Assert.NotNull(fetchedCustomer);
+        Assert.Equal("Happy", fetchedCustomer.Name);
+        Assert.Equal("happy@testmail.de", fetchedCustomer.Email);
+
+    }
+
 }
